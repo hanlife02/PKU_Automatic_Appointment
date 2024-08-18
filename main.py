7e308998b570d44e9848550e6e03e9475cc5a490
Ethan <150881448+hanlife02@users.noreply.github.com>
2024-08-18 14:06:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import time
 import threading
-import subprocess
 from datetime import datetime
 from dotenv import load_dotenv
+from make_appointment import make_appointment
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 
 # 加载.env文件中的环境变量
 load_dotenv()
@@ -14,9 +16,31 @@ day = int(os.getenv('DAY'))
 hour = int(os.getenv('HOUR'))
 min = int(os.getenv('MIN'))
 
-# 运行你的 appointment.py 脚本
-def run_appointment_script():
-    subprocess.run(["python", "appointment.py"])
+username_str = os.getenv('MY_USERNAME')
+password_str = os.getenv('PASSWORD')
+edgedriver_path = os.getenv('EDGEDRIVER_PATH')
+number = int(os.getenv('NUMBER'))  # 确保转换为整数
+
+date_first = os.getenv('First_DATE_TEXT')
+door_first = os.getenv('First_DOOR_TEXT')
+time_first = os.getenv('First_TIME_TEXT')
+name_first = os.getenv('First_NAME')
+id_number_first = os.getenv('First_ID_NUMBER')
+phone_first = os.getenv('First_PHONE')
+purpose_first = os.getenv('First_PURPOSE')
+
+date_second = os.getenv('Second_DATE_TEXT')
+door_second = os.getenv('Second_DOOR_TEXT')
+time_second = os.getenv('Second_TIME_TEXT')
+name_second = os.getenv('Second_NAME')
+id_number_second = os.getenv('Second_ID_NUMBER')
+phone_second = os.getenv('Second_PHONE')
+purpose_second = os.getenv('Second_PURPOSE')
+
+secret = os.getenv('SECRET')
+
+# 配置EdgeDriver路径
+service = Service(edgedriver_path)
 
 # 计算到特定日期的剩余秒数
 def calculate_seconds_until(target_datetime):
@@ -25,7 +49,7 @@ def calculate_seconds_until(target_datetime):
     return delta.total_seconds()
 
 # 设置目标日期和时间 (年, 月, 日, 时, 分)
-target_date = datetime(year, month, day, hour, min)  # 修改为你需要的日期和时间
+target_date = datetime(year, month, day, hour, min)
 
 # 创建一个线程来等待到指定时间并运行脚本
 def schedule_task():
@@ -42,7 +66,20 @@ def schedule_task():
             time.sleep(time_until_target)
             time_until_target = 0
 
-    run_appointment_script()
+    # 第一次预约，关闭浏览器
+    driver = webdriver.Edge(service=service)
+    make_appointment(driver, date_first, door_first, time_first, name_first, id_number_first, phone_first, purpose_first, secret, close_driver=True)
+
+    # 如果有第二次预约，重新启动浏览器，并预约
+    if number == 2:
+        # 重启浏览器
+        driver = webdriver.Edge(service=service)
+        time.sleep(0.3)
+        make_appointment(driver, date_second, door_second, time_second, name_second, id_number_second, phone_second, purpose_second, secret, close_driver=True)
+
+    # 结束后等待五分钟再关闭浏览器
+    time.sleep(300)
+    driver.quit()
 
 # 启动线程
 thread = threading.Thread(target=schedule_task)
